7846c013e83c6b6ecb0efdb1743051719282fac3
jaehun-jeong <keepthefaith00@gmail.com>
2022-03-17 20:57:25 +0900

    문제 추가

---
+++ src/jh/javabookcodes/FileTest2.java
@@ -0,0 +1,22 @@
+package jh.javabookcodes;
+
+public class FileTest2 {
+    public static void main(String[] args) {
+        /*
+        * 문제 : 1~10 까지의 숫자를 입력받아
+        * TESTX.txt에 해당하는 파일의 정보를 보여주시오.
+        *
+        * 입력 :
+        * 3
+        * 출력 :
+        * 파일명: TEST3.txt, 파일크기 : 6bytes, 파일 경로: C:\TEST\TEST3.txt
+        *
+        * --------------
+        *
+        * 입력 :  1~ 10 이외 다른 숫자, 문자일 경우
+        * 출력 : 
+        * 1~10 사이의 숫자만 입력하세요.
+        * 종료.
+        * */
+    }
+}
