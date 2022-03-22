d04e76481a952de4e0d37c2ed20db58d5e044c4b
jaehun-jeong <keepthefaith00@gmail.com>
2022-03-22 20:33:12 +0900

    option문제추가

--- src/jh/javabookcodes/FileTest2.java
+++ src/jh/javabookcodes/FileTest2.java
@@ -4,7 +4,22 @@ import java.io.File;
 import java.util.Scanner;
 
 public class FileTest2 {
-
+    /*
+     * 문제 : 1~10 까지의 숫자를 입력받아 (option문제 : TEST/ 디렉토리 안에 있는 파일의 갯수만큼의 숫자를 입력받아)
+     * TESTX.txt에 해당하는 파일의 정보를 보여주시오.
+     *
+     * 입력 :
+     * 3
+     * 출력 :
+     * 파일명: TEST3.txt, 파일크기 : 6bytes, 파일 경로: C:\TEST\TEST3.txt
+     *
+     * --------------
+     *
+     * 입력 :  1~ 10 이외 다른 숫자, 문자일 경우
+     * 출력 :
+     * 1~10 사이의 숫자만 입력하세요.
+     * 종료.
+     * */
     public static void main(String[] args) {
         System.out.print("파일이름 입력: ");
         String input = (new Scanner(System.in)).nextLine();
